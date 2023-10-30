782f69941815fa4bcf02ad656b69811dc32b41dc
Junhua <wasd218312@gmail.com>
2023-10-30 17:16:39 +0000

    update repo

--- Main.js
+++ Main.js
@@ -1,21 +1,10 @@
-// export default class Main extends Phaser.Scene {
-//     constructor() {
-//         super("Main");
-//     }
-
-const game = new Phaser.Game(config);
-
-     function preload() {
-        this.load.image('MapEditor', 'tiles/test_map.json')
-        this.load.tilemapTiledJSON('MapEditor', 'tiles/test_map.json')
+class Main extends Phaser.Scene {
+    constructor(){
+        super(Main);
     }
-    create() {
-	this.add.image(0, 0, 'base_tiles')
-    }
+
+    
 }
 
 
-
-
-
-
+export default Main
